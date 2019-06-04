fe69595b0e4ab1ac2ecf9f221fc05a35199897ff
Sobhit Kumar <sobhit@tadnyasoftech.com>
2019-06-04 09:24:27 +0530

    Adding Style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,9 +5,41 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  /**
+   * Style in ReactJS can be:
+   * 1. inline -- use double curly braces style = {{backgroundColor: '#f4f4f4'}}
+   * 2. as const variable   -- need only single curly braces
+   * const itemStyle= {backgroundColor: '#f4f4f4'} ;   use as style={itemStyle}>
+   * 3. can also use as a function
+   *               getStyle = () =>{
+                if(this.props.todo.completed) {
+                  if(this.props.todo.completed){
+                    return {
+                      textDecoration: 'line-through'
+                    }
+                  }else {
+                    return {
+                      textDecoration: 'none'
+                    }
+                  }
+                }
+              }
+
+   * and use as style={this.getStyle()}>
+   */
+
+   getStyle = () =>{
+      return {
+        backgroundColor: '#f4f4f4',
+        padding: '10px',
+        marginTop: '4px',
+        borderBottom: '1px #ccc dotted',
+        textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+      }
+   }
   render() {
     return (
-      <div>
+      <div style={this.getStyle()}>
         <h3>{this.props.todo.title}</h3>
       </div>
     )
@@ -18,4 +50,4 @@ TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
 
-export default TodoItem
+export default TodoItem;
