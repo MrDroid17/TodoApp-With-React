6d717d42c26083ef586d476a756aab55513b8eba
Sobhit Kumar <sobhit@tadnyasoftech.com>
2019-06-05 17:20:17 +0530

    apply styling to the header

--- src/App.js
+++ src/App.js
@@ -52,4 +52,6 @@ class App extends Component {
   }
 }
 
+
+
 export default App;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>
         Todo List
       </h1>
     </header>
   )
+}
+
+const headerStyle = {
+  background: '#000000',
+  padding: '10px',
+  textAlign: 'center',
+  color: '#FFFFFF'
 }
 
 export default Header;
