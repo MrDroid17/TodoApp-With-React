d7d9b8246c938a6f8c313424f096ab98c38e0677
Sobhit Kumar <sobhit@tadnyasoftech.com>
2019-06-09 02:38:58 +0530

    addTodo and deleteTodo api added with additional proptypes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
-import uuid from 'uuid';
 import About from './components/pages/About';
 import Axios from 'axios';
 
@@ -35,20 +34,22 @@ class App extends Component {
 
   // delete todo
   delTodo = (id) => {
-    this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res =>{
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
     })
   }
 
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
-      title,
-      completed: false
-    }
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    })
+   Axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false})
+   .then(res=>{
+
+     this.setState({
+       todos: [...this.state.todos, res.data]
+     })
+   })
   }
 
   render() {
