14aa7f3fce6981a82082ebdc8dc76a91eac8992a
Sobhit Kumar <sobhit@tadnyasoftech.com>
2019-06-09 01:13:38 +0530

    adding react-router for about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h2>About me</h2>
+      <p>This is a React Todo App.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dolore cumque quia suscipit iure atque ea distinctio quis laboriosam quam corporis quo aut est vero blanditiis. Sed mollitia voluptas beatae in cum, soluta cupiditate excepturi voluptates, incidunt impedit optio! Cum, animi aliquid enim corporis voluptas temporibus, quis possimus provident eligendi tempore, molestiae inventore libero repellendus dicta voluptates quia? Consequuntur, inventore quo neque magnam repellendus nobis ipsam error nemo repudiandae voluptatibus, totam ut iusto quae ad nostrum eos possimus dolorem vitae, odio quaerat! Atque adipisci inventore, fuga id veniam alias reiciendis similique, impedit magni officia at esse architecto veritatis exercitationem laborum, excepturi rerum laboriosam quo sapiente. Ex maxime voluptatibus a ipsum et recusandae esse harum culpa tempore accusamus? Aliquam aut ex eligendi beatae itaque et cupiditate quasi expedita distinctio tempore quibusdam alias a necessitatibus voluptatum quis labore quo vel, eum laboriosam aspernatur non! Animi asperiores fugit eligendi expedita quasi! Sunt obcaecati voluptatum, laudantium praesentium beatae odio esse, ab ducimus reiciendis, ipsa rem unde repellat labore corrupti architecto. Necessitatibus blanditiis distinctio qui ex aliquam, iste fugit alias ut iure sit consequuntur molestiae minima adipisci? Nostrum libero dicta eveniet ullam ea recusandae ab repudiandae cumque vero ex saepe, ipsum ipsam tenetur quidem velit at et perferendis esse iste nemo reiciendis dolor minima illo reprehenderit? Doloribus eos illo magnam molestias! Est deleniti minima voluptatum molestias illum temporibus dicta. Quasi numquam ratione eos soluta minima nulla, in fugit dolores asperiores ipsum voluptate similique laudantium, consequatur incidunt? Eveniet natus distinctio explicabo quo libero eos dolorum amet?</p>
+    </React.Fragment>
+  )
+}
